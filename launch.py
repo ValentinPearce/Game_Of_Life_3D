d086ee8e7997a2630c40bd7515e4a43987b05d77
ValentinPearce <valentinpearce@free.fr>
2014-02-08 18:09:24 +0100

    Deleted example from the launcher.

--- launch.py
+++ launch.py
@@ -7,14 +7,11 @@ print("Conway's Game Of Life is a mathematic cell life simulation.")
 print("It models the evolution of single cell generations in a table.")
 print("My idea was to transpose this problem in a three dimension matrix")
 print("=================================================================")
-print("Here is a small demonstration of the evolution of the following matrix :")
-gol.createTab(3)
-
-gol.live(0,0,0)
-gol.live(1,0,0)
-gol.live(0,1,0)
-gol.live(0,0,1)
-gol.live(1,1,1)
+i = 0
+j = 0
+k = 0
+size = input("Please input the size of one side of the grid : ")	
+gol.createTab(size)
 print tab
-gol.generation(3)
-print gol.tab
+gol.generation(size)
+print tab
